2cd0519cee2f334ef8d81b7b99e627fc8001c3cc
Ryan LaClair <ryan@donut-studios.com>
2017-03-30 15:03:04 -0400

    Added slide transitions, refactored callbacks

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,26 +1,65 @@
-// check off item when clicked
-$("ul").on("click", "li", function() {
-  $(this).toggleClass("completed");
-});
+$(document).ready(function() {
 
-// click to delete item
-$("ul").on("click", "span", function(event) {
-  $(this).parent().fadeOut(500, function() {
-    $(this).parent().remove();
+  $(".fa-plus").on("click", showInputBox);
+
+  $(document).on("click", hideInputBox);
+
+  // block propagation of clicks inside input box
+  $("input[type='text']").click(function(event) {
+    event.stopPropagation();
+  })
+
+  $("ul").on("click", "li", crossOffListItem);
+
+  $("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+      saveListItem();
+    }
   });
-  event.stopPropogation();
+
+  $("ul").on("click", ".remove", deleteListItem);
+
 });
 
-// add an item
-$("input[type='text']").keypress(function(event) {
-  if (event.which === 13) {
-    var newItem = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span class='hoverTarget'><span class='remove'><i class='fa fa-minus'></i></span> </span>" + newItem + "</li>")
+function showInputBox(event) {
+  event.stopPropagation();
+
+  if ($("#input-container").is(":visible")) {
+    saveListItem();
   }
-});
+  else {
+    $("#input-container").slideDown(150);
+    $("input[type='text']").focus();
+  }
+}
 
-// click to add new item
-$(".fa-plus").on("click", function() {
-  $("input[type='text']").fadeToggle();
-})
+function hideInputBox(event) {
+  if (this.id != "list-container") {
+    if ($("#input-container").is(":visible")) {
+      $("#input-container").slideUp(150);
+   }
+  }
+}
+
+function crossOffListItem() {
+  $(this).toggleClass("completed");
+}
+
+function saveListItem() {
+  var newItem = $("input[type='text']").val();
+  $("input[type='text']").val("");
+  $("ul").append("<li id='new-item'><span class='hover-target'><span class='remove'><i class='fa fa-minus'></i></span> </span>" + newItem + "</li>");
+  
+  $("#new-item").slideDown(150);
+  $("#new-item").removeAttr("id");
+}
+
+function deleteListItem(event) {
+  event.stopPropagation();
+
+  $(this).parent().parent().slideUp(150, function() {
+    $(this).remove();
+  });
+
+  $("input[type='text']").focus();
+}
