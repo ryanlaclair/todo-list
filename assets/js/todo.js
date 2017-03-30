fcadf5a19f94d1135234a524a6151bb0964e7b08
Ryan LaClair <ryan@donut-studios.com>
2017-03-30 15:14:00 -0400

    Added starting items with directions

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,17 +1,67 @@
 $(document).ready(function() {
 
+  var inputContainer = $("#input-container");
+  var inputBox = $("input[type='text']");
+
+  // FUNCTIONS
+
+  function showInputBox(event) {
+    event.stopPropagation();
+
+    if (inputContainer.is(":visible")) {
+      saveListItem();
+    }
+    else {
+      inputContainer.slideDown(150);
+      inputBox.focus();
+    }
+  }
+
+  function hideInputBox(event) {
+    if (this.id != "list-container") {
+      if (inputContainer.is(":visible")) {
+        inputContainer.slideUp(150);
+     }
+    }
+  }
+
+  function crossOffListItem() {
+    $(this).toggleClass("completed");
+  }
+
+  function saveListItem() {
+    var newItem = inputBox.val();
+    inputBox.val("");
+    $("ul").append("<li id='new-item'><span class='hover-target'><span class='remove'><i class='fa fa-minus'></i></span> </span>" + newItem + "</li>");
+    
+    $("#new-item").slideDown(150);
+    $("#new-item").removeAttr("id");
+  }
+
+  function deleteListItem(event) {
+    event.stopPropagation();
+
+    $(this).parent().parent().slideUp(150, function() {
+      $(this).remove();
+    });
+
+    inputBox.focus();
+  }
+
+  // DOCUMENT CONTROL
+
   $(".fa-plus").on("click", showInputBox);
 
   $(document).on("click", hideInputBox);
 
   // block propagation of clicks inside input box
-  $("input[type='text']").click(function(event) {
+  inputBox.click(function(event) {
     event.stopPropagation();
-  })
+  });
 
   $("ul").on("click", "li", crossOffListItem);
 
-  $("input[type='text']").keypress(function(event) {
+  inputBox.keypress(function(event) {
     if (event.which === 13) {
       saveListItem();
     }
@@ -21,45 +71,3 @@ $(document).ready(function() {
 
 });
 
-function showInputBox(event) {
-  event.stopPropagation();
-
-  if ($("#input-container").is(":visible")) {
-    saveListItem();
-  }
-  else {
-    $("#input-container").slideDown(150);
-    $("input[type='text']").focus();
-  }
-}
-
-function hideInputBox(event) {
-  if (this.id != "list-container") {
-    if ($("#input-container").is(":visible")) {
-      $("#input-container").slideUp(150);
-   }
-  }
-}
-
-function crossOffListItem() {
-  $(this).toggleClass("completed");
-}
-
-function saveListItem() {
-  var newItem = $("input[type='text']").val();
-  $("input[type='text']").val("");
-  $("ul").append("<li id='new-item'><span class='hover-target'><span class='remove'><i class='fa fa-minus'></i></span> </span>" + newItem + "</li>");
-  
-  $("#new-item").slideDown(150);
-  $("#new-item").removeAttr("id");
-}
-
-function deleteListItem(event) {
-  event.stopPropagation();
-
-  $(this).parent().parent().slideUp(150, function() {
-    $(this).remove();
-  });
-
-  $("input[type='text']").focus();
-}
